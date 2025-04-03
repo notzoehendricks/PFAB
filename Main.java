b52d319f03646e69edfa1fd36a9207b74e769773
notzoehendricks <zoe.hendricks16@gmail.com>
2025-04-03 09:26:33 +0200

    This is my final lab1 practice from PFAB. Basic functions and varaible were created to solidify the basics

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+  public static void main(String[] args) {
+      String first = "Zoe";  
+      String last = "Hendricks";    
+      int age = 22;           
+
+      System.out.println(first + " " + last + ", Age: " + age);
+
+      double sideA = 12.5;
+      double sideB = 17.85;
+      double sideC = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+      System.out.println("The length of the hypotenuse (sideC) is: " + sideC);
+
+
+      double operand1 = 95;
+      double operand2 = 64.5;
+
+      System.out.println(operand1 + " + " + operand2 + " = " + (operand1 + operand2));
+      System.out.println(operand1 + " - " + operand2 + " = " + (operand1 - operand2));
+      System.out.println(operand1 + " * " + operand2 + " = " + (operand1 * operand2));
+      System.out.println(operand1 + " / " + operand2 + " = " + (operand1 / operand2));
+      System.out.println(operand1 + " % " + operand2 + " = " + (operand1 % operand2));
+
+  }
+}
